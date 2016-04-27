a92439010fe23acaa218496d16654304270cca14
Gaurav <gauravsarma1992@gmail.com>
2016-04-27 10:21:37 +0530

    Added graph

---
+++ app/assets/javascripts/graphApp.js
@@ -0,0 +1,11 @@
+var fortuneGraph= angular.module('fortuneGraph',['highcharts-ng']);
+fortuneGraph.controller('graphController',['$scope', 'Graph', function($scope, Graph){
+    $scope.showModal =  false;
+    console.log("worknation")
+
+    $scope.scatter = Graph.scatter();
+    $scope.bar = Graph.bar();
+    $scope.stackedColumns = Graph.stackedColumns();
+    $scope.pie = Graph.pie({ titleText: 'Skills' })
+
+}])
